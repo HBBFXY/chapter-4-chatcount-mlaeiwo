72026b3b850b44a8740ab44330f4e987f4cf70fe
mlaeiwo <2186539832@qq.com>
2025-10-19 09:49:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-# 初始化各类字符的计数变量
 letter = 0
 digit = 0
 space = 0
 other = 0
-
-# 获取键盘输入的一行字符
 s = input("请输入一行字符：")
-
-# 遍历输入的每一个字符
 for c in s:
-    if c.isalpha():  # 判断是否为英文字符（字母）
+    if c.isalpha():
         letter += 1
-    elif c.isdigit():  # 判断是否为数字
+    elif c.isdigit():
         digit += 1
-    elif c.isspace():  # 判断是否为空格
+    elif c.isspace():
         space += 1
-    else:  # 其余的为其他字符
+    else:
         other += 1
-
-# 按照格式输出统计结果
 print(f"英文字符: {letter}")
 print(f"数字: {digit}")
 print(f"空格: {space}")
-print(f"其他字符: {other}")# 这个文件用于编写代码
+print(f"其他字符: {other}")
